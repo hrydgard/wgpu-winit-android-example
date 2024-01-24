44a3dbf54fe35b408ba7105d7b465bdd421dc83e
Henrik Rydgård <hrydgard@gmail.com>
2024-01-24 17:57:35 +0100

    Add audio playback
    Remove some irrelevant stuff

--- lib/src/app.rs
+++ lib/src/app.rs
@@ -1,7 +1,10 @@
 use crate::framework::Example;
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    FromSample, Sample, SizedSample, Stream,
+};
 use std::time::Instant;
 use wgpu::{ColorTargetState, ColorWrites};
-use winit::event::WindowEvent;
 
 const SHADER: &str = r#"
 @vertex
@@ -20,6 +23,46 @@ fn fs_main() -> @location(0) vec4<f32> {
 pub struct App {
     last_time: Instant,
     render_pipeline: wgpu::RenderPipeline,
+    stream: Stream,
+}
+
+pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<Stream, anyhow::Error>
+where
+    T: SizedSample + FromSample<f32>,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // Produce a sinusoid of maximum amplitude.
+    let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+        None,
+    )?;
+    Ok(stream)
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: Sample + FromSample<f32>,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = T::from_sample(next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
 }
 
 impl Example for App {
@@ -63,8 +106,37 @@ impl Example for App {
             multiview: None,
         });
 
+        let host = cpal::default_host();
+
+        let device = host.default_output_device().unwrap();
+        println!("Output device: {}", device.name().unwrap());
+
+        let config = device.default_output_config().unwrap();
+        println!("Default output config: {:?}", config);
+
+        let stream = match config.sample_format() {
+            cpal::SampleFormat::I8 => run::<i8>(&device, &config.into()),
+            cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
+            // cpal::SampleFormat::I24 => run::<I24>(&device, &config.into()),
+            cpal::SampleFormat::I32 => run::<i32>(&device, &config.into()),
+            // cpal::SampleFormat::I48 => run::<I48>(&device, &config.into()),
+            cpal::SampleFormat::I64 => run::<i64>(&device, &config.into()),
+            cpal::SampleFormat::U8 => run::<u8>(&device, &config.into()),
+            cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
+            // cpal::SampleFormat::U24 => run::<U24>(&device, &config.into()),
+            cpal::SampleFormat::U32 => run::<u32>(&device, &config.into()),
+            // cpal::SampleFormat::U48 => run::<U48>(&device, &config.into()),
+            cpal::SampleFormat::U64 => run::<u64>(&device, &config.into()),
+            cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
+            cpal::SampleFormat::F64 => run::<f64>(&device, &config.into()),
+            sample_format => panic!("Unsupported sample format '{sample_format}'"),
+        }
+        .unwrap();
+
+        stream.play().unwrap();
         Self {
             render_pipeline,
+            stream,
             last_time: Instant::now(),
         }
     }
@@ -78,7 +150,7 @@ impl Example for App {
         // do nothing
     }
 
-    fn window_event(&mut self, _event: WindowEvent) {
+    fn window_event(&mut self, _event: winit::event::WindowEvent) {
         // Do nothing
     }
 
